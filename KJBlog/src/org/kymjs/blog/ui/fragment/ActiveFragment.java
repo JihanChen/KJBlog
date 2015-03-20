77b28c47af7cece901234d58aedf4c91149067e4
kymjs <kymjs123@gmail.com>
2015-03-20 19:12:07 +0800

    .

---
+++ KJBlog/src/org/kymjs/blog/ui/fragment/ActiveFragment.java
@@ -0,0 +1,21 @@
+package org.kymjs.blog.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * [猿活动]界面
+ * 
+ * @author kymjs
+ * 
+ */
+public class ActiveFragment extends TitleBarFragment {
+
+    @Override
+    protected View inflaterView(LayoutInflater inflater, ViewGroup container,
+            Bundle bundle) {
+        return null;
+    }
+}
