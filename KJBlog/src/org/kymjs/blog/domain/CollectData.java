aaccaeff0316577759cee1a492a77183c9b56320
kymjs <kymjs123@gmail.com>
2015-04-20 23:10:03 +0800

    添加侧滑菜单

---
+++ KJBlog/src/org/kymjs/blog/domain/CollectData.java
@@ -0,0 +1,38 @@
+package org.kymjs.blog.domain;
+
+/**
+ * 我的收藏，javabean
+ * 
+ * @author kymjs
+ * 
+ */
+public class CollectData {
+    private int id;
+    private String name;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
